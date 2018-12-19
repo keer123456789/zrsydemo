6afcab05410fae7a6c94751931321edd2a2aea9d
keer123456789 <15110074528@163.com>
2018-12-19 20:16:31 +0800

    1

--- src/main/java/com/sjcl/zrsy_demo/domain/PigSelfInfo.java
+++ src/main/java/com/sjcl/zrsy_demo/domain/PigSelfInfo.java
@@ -1,13 +1,13 @@
 package com.sjcl.zrsy_demo.domain;
 
+
 public class PigSelfInfo {
+
     private String id ;
     private String temperature;
-//    private String weight;
+
     private String datetime;
 
-    public PigSelfInfo() {
-    }
 
     public String getId() {
         return id;
@@ -24,15 +24,6 @@ public class PigSelfInfo {
     public void setTemperature(String temperature) {
         this.temperature = temperature;
     }
-
-//    public String getWeight() {
-//        return weight;
-//    }
-//
-//    public void setWeight(String weight) {
-//        this.weight = weight;
-//    }
-
 
     public String getDatetime() {
         return datetime;
